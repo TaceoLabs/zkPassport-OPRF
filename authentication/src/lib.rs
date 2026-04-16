2af4ee982106874ca3845db95a6e9606dc954967
saleel <saleel@saleel.xyz>
2026-04-16 16:32:43 +0400

    chore: set module name as zkpassport

--- authentication/src/lib.rs
+++ authentication/src/lib.rs
@@ -25,7 +25,7 @@ pub enum AuthModules {
 
 impl core::fmt::Display for AuthModules {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("face")
+        f.write_str("zkpassport")
     }
 }
 

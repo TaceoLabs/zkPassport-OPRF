2a10a7cd74eb8c2d76738218a5489de551c368d8
saleel <saleel@saleel.xyz>
2026-07-15 23:37:34 +0530

    update tags

--- test-utils/src/containers.rs
+++ test-utils/src/containers.rs
@@ -12,7 +12,7 @@ use tokio::sync::OnceCell;
 
 const PROOF_VERIFIER_IMAGE: &str =
     "europe-west2-docker.pkg.dev/proof-verifier/proof-verifier/proof-verifier";
-const PROOF_VERIFIER_TAG: &str = "57456d6dca5af880a0228bd79f069ce8ed13e18a";
+const PROOF_VERIFIER_TAG: &str = "ce394052661a4576b143c9ec83cb8012b47c8944";
 const PROOF_VERIFIER_INTERNAL_PORT: u16 = 8080;
 
 static PROOF_VERIFIER: OnceCell<(Url, ContainerAsync<GenericImage>)> = OnceCell::const_new();
